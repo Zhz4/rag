c74b2c4fd0ff14950e1fc465d88f8f94b20eb198
zhanghuizhong <1057213071@qq.com>
2025-02-22 23:59:25 +0800

    Revert "perf：让大模型只输出回答内容"
    This reverts commit 673fba656a07641c62206e428e7ca36c0bb308d0.

--- app/utils/handlers.py
+++ app/utils/handlers.py
@@ -9,16 +9,10 @@ class StreamingHandler(BaseCallbackHandler):
     def __init__(self):
         self.queue = asyncio.Queue()
         self.source_documents = None
-        self.is_answer_phase = False  # 添加标志来跟踪是否在答案阶段
-
-    async def on_llm_start(self, *args, **kwargs) -> None:
-        """当 LLM 开始生成时被调用"""
-        self.is_answer_phase = True
 
     async def on_llm_new_token(self, token: str, **kwargs) -> None:
         """处理流式输出的每个token"""
-        if self.is_answer_phase:  # 只有在答案阶段才输出token
-            await self.queue.put(token)
+        await self.queue.put(token)
 
     def create_sse_event(self, token, is_source=False):
         """创建 SSE 事件"""
